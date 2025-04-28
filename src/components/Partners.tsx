399822476554beea5573626447239dbb1993fadf
Parmesh119 <parmeshb90@gmail.com>
2025-04-28 14:56:40 +0530

    added partners and friends logo

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,10 +1,16 @@
 import { Button } from '@/components/ui/button';
 import { UserRound } from 'lucide-react';
-
+import AWS from '@/assets/Partners/AWS.png';
+import GoogleCloud from '@/assets/Partners/GoogleCloud.png';
+import August_Brown from "@/assets/Partners/August_Brown.png";
+import MS_AZ from "@/assets/Partners/MS_AZ.png";
+import FOR_M from "@/assets/Friends/FOR_M.png";
+import Gener8tor from "@/assets/Friends/Generator.png";
+import WISC from "@/assets/Friends/WISC.png";
 export function Partners() {
   return (
     <section className="w-full py-20 bg-gradient-to-r from-[#FFFFFF] to-[#F0F0F0]">
-      <div className="container mx-auto max-w-6xl px-4 md:px-8">
+      <div className="container mx-auto max-w-6xl">
         <div className="text-center mb-20">
           <div className="inline-block mb-4 px-6 py-1 bg-[#9DC22333] text-[#2e8318] rounded-full text-sm font-medium">
             <span className='flex flex-row gap-2'><UserRound className='h-5 w-4 border-dark' /> Partners</span>
@@ -13,13 +19,13 @@ export function Partners() {
             Who we're <span className="text-[#2D7DD2]">partnered with</span>
           </h2>
           <p className="text-gray-600 max-w-2xl mx-auto">
-            Lorem ipsum Lorem ipsum Lorem ipsum 
+            Lorem ipsum Lorem ipsum Lorem ipsum
           </p>
-          <div className="grid grid-cols-2 md:grid-cols-4 gap-24 items-center mt-12 opacity-60">
-            <img src="https://kaskade.cloud/wp-content/uploads/2020/08/aws-logo.png" alt="AWS" className="h-36 w-auto mx-auto" />
-            <img src="https://logos-world.net/wp-content/uploads/2021/02/Google-Cloud-Logo.png" alt="Google Cloud" className="h-32 mx-auto w-64" />
-            <img src="https://media.licdn.com/dms/image/v2/D560BAQFCIa7TE6AOYQ/company-logo_200_200/company-logo_200_200/0/1664420970990/augustbrown4u_logo?e=2147483647&v=beta&t=29cuWFRJpbBRMxubPO2aF9AWtLYK_-4NX1a2z7nIEac" alt="August Brown" className="h-36 w-40 mx-auto" />
-            <img src="https://neoris.com/documents/20126/41386/microsoft-azure-logo+%281%29.jpg/706fd113-e379-dbe7-560f-a63c59a01856?version=1.0&t=1644322743646&imagePreview=1" alt="Microsoft Azure" className="h-36 w-56 mx-auto" />
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8 items-center mt-12 opacity-60">
+            <img src={AWS} alt="AWS" className="h-36 w-auto mx-auto" />
+            <img src={GoogleCloud} alt="Google Cloud" className="h-32 mx-auto w-full" />
+            <img src={August_Brown} alt="August Brown" className="h-full w-full mx-auto" />
+            <img src={MS_AZ} alt="Microsoft Azure" className="h-full w-full mx-auto" />
           </div>
         </div>
         <div className="text-center mb-20">
@@ -29,10 +35,10 @@ export function Partners() {
           <h2 className="text-3xl md:text-4xl font-bold mb-12">
             We've got <span className="text-[#2D7DD2]">acolades from</span>
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-3 gap-12 items-center opacity-60">
-            <div className="text-2xl font-bold text-gray-600">FOR-M by mke tech</div>
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdrybBJsaEdn3WrSk3PGEre7nYB4BxM99TWw&s" alt="gener8tor" className="h-16 w-64 mx-auto" />
-            <img src="https://www.momentumwest.org/media/userfiles/subsite_16/images/bd/wedc_rev.jpg" alt="WISCONSIN ECONOMIC DEVELOPMENT" className="h-36 w-56 mx-auto" />
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-8 items-center opacity-60">
+            <img src={FOR_M} alt="FOR-M by mke tech" className="h-28 w-64 mx-auto" />
+            <img src={Gener8tor} alt="gener8tor" className="h-28 w-64 mx-auto" />
+            <img src={WISC} alt="WISCONSIN ECONOMIC DEVELOPMENT" className="h-28 w-64 mx-auto" />
           </div>
         </div>
       </div>
